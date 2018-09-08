c305c3036ac387712a003d12b67461d8a3aa7b88
Schiriac Robert <schiriac.robert@gmail.com>
2018-09-08 13:07:49 +0300

    added email notification via gmail

--- app.js
+++ app.js
@@ -1,9 +1,43 @@
+require('dotenv').config();
 const whois = require('whois-to-json');
 const chalk = require('chalk');
 const notifier = require('node-notifier');
 const path = require('path');
 const config = require(path.join(__dirname, 'config.json'));
 const domains = require(path.join(__dirname, config.domainsFile));
+const sendMail = require('gmail-send')(config.email);
+
+const envVars = [
+  "DOMAIN_HUNTER_EMAIL_USER", 
+  "DOMAIN_HUNTER_EMAIL_PASS",
+  "DOMAIN_HUNTER_EMAIL_TO"
+];
+
+config.notifyByEmail = envVars.filter(k => process.env[k] && (process.env[k].trim().length > 0) ).length === envVars.length;
+
+const notify = (domain) => {
+  if( config.notifyByEmail ) {
+    const wildcard = /%DOMAIN%/gm;
+    
+    sendMail({
+      user: process.env["DOMAIN_HUNTER_EMAIL_USER"],
+      pass: process.env["DOMAIN_HUNTER_EMAIL_PASS"],
+      to: process.env["DOMAIN_HUNTER_EMAIL_TO"],
+      subject: config.email.subject.replace(wildcard, domain),
+      html: config.email.htmlData.map(l => l.replace(wildcard, domain)).join("<br>")
+    }, (err, res) => {
+      err ? 
+        console.error(chalk.bold.red(`Error while sending email: ${err}`)) : 
+        console.log(chalk.bold.blue(`Email sent successfully!`));
+    });
+  } else {
+    notifier.notify({
+      ...config.notify,
+      message: `${domain} is available! Buy now!`,
+      icon: path.join( __dirname, 'dhunter.png'),
+    });
+  }
+}
 
 const checkDomain = (domain) => {
   whois(domain)
@@ -11,13 +45,7 @@ const checkDomain = (domain) => {
     .then((domainStatus) => {
       if( typeof domainStatus === "undefined") {
         console.log(`[${chalk.bold.blue(domain)}] : ${chalk.bold.green('AVAILABLE')}`);
-  
-        notifier.notify({
-          ...config.notify,
-          message: `${domain} is available! Buy now!`,
-          icon: path.join( __dirname, 'dhunter.png'),
-        });
-        
+        notify(domain);
       } else {
         console.log(`[${chalk.bold.blue(domain)}] : ${chalk.bold.gray(domainStatus)}`);
         checkDomain(domain);
