04ac0e82ea49a7faf7e12b79d057f33a37581197
Schiriac Robert <schiriac.robert@gmail.com>
2018-09-09 21:14:08 +0300

    added TS (multiple changes)

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,31 @@
-require('dotenv').config();
-const whois = require('whois-to-json');
-const chalk = require('chalk');
-const notifier = require('node-notifier');
-const path = require('path');
+import dotenv from 'dotenv';
+import chalk from 'chalk';
+import notifier from 'node-notifier';
+import path from 'path';
+import config from './static/config.json';
+import domains from './static/domains.json';
+import whois from 'whois-to-json';
+import sendMail from 'gmail-send';
 
-const config = require(path.join(__dirname, 'config.json'));
-const domains = require(path.join(__dirname, config.domainsFile));
-const sendMail = require('gmail-send')(config.email);
+dotenv.config();
 
-const envVars = [
+const envVars: string[] = [
   'DOMAIN_HUNTER_EMAIL_USER',
   'DOMAIN_HUNTER_EMAIL_PASS',
   'DOMAIN_HUNTER_EMAIL_TO'
 ];
 
-config.notifyByEmail = envVars
-  .filter(k => process.env[k] && process.env[k].trim().length > 0)
+const notifyByEmail: boolean = envVars
+  .filter((k: string) => process.env[k] && (process.env[k] as string).trim().length > 0)
   .length === envVars.length;
 
-const notify = domain => {
-  if (config.notifyByEmail) {
-    const wildcard = /%DOMAIN%/gm;
+const notify = ( domain: string ) => {
+  if ( notifyByEmail ) {
+    const wildcard: RegExp = /%DOMAIN%/gm;
 
     sendMail(
       {
+        ...config.email,
         user: process.env.DOMAIN_HUNTER_EMAIL_USER,
         pass: process.env.DOMAIN_HUNTER_EMAIL_PASS,
         to: process.env.DOMAIN_HUNTER_EMAIL_TO,
@@ -32,7 +34,7 @@ const notify = domain => {
           .map(l => l.replace(wildcard, domain))
           .join('<br>')
       },
-      (err) => {
+      (err: null | object) => {
         err
           ? console.error(chalk.bold.red(`Error while sending email: ${err}`))
           : console.log(chalk.bold.blue('Email sent successfully!'));
@@ -41,16 +43,16 @@ const notify = domain => {
   } else {
     notifier.notify({
       ...config.notify,
-      message: `${domain} is available! Buy now!`,
-      icon: path.join(__dirname, 'assets', 'dhunter.png')
+      icon: path.join(__dirname, 'src', 'assets', 'dhunter.png'),
+      message: `${domain} is available! Buy now!`
     });
   }
 };
 
-const checkDomain = domain => {
-  whois(domain)
-    .then(response => response.DomainStatus)
-    .then(domainStatus => {
+const checkDomain = ( domain: string) => {
+  whois( domain )
+    .then(( response: any ) => response.DomainStatus)
+    .then(( domainStatus: string | undefined ) => {
       if (typeof domainStatus === 'undefined') {
         console.log(
           `[${chalk.bold.blue(domain)}] : ${chalk.bold.green('AVAILABLE')}`
@@ -63,7 +65,7 @@ const checkDomain = domain => {
         checkDomain(domain);
       }
     })
-    .catch(error => {
+    .catch(( error: object ) => {
       console.error(`[${chalk.bold.red('ERROR')}]: ${JSON.stringify(error)}`);
       checkDomain(domain);
     });
---
+++ src/typings.d.ts
@@ -0,0 +1,14 @@
+declare module "*.json" {
+  const value: any;
+  export default value;
+}
+
+declare module "whois-to-json" {
+  const value: any;
+  export default value;
+}
+
+declare module "gmail-send" {
+  const value: any;
+  export default value;
+}
